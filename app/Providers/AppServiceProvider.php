31eb50dc33da6390ea0227eec5fad6720d2d0b86
Roman Loboda <r.loboda@ideil.com>
2017-05-28 19:42:17 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,16 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
+
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //app('App\Http\Controllers\TimelineController')->createFirstNotification();
+        Schema::defaultStringLength(191);
     }
 
     /**
